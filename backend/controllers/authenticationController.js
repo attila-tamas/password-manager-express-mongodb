04e857609f6f3a3653594dcfb5be62ceb7512c73
attila-tamas <tamasattila02@gmail.com>
2023-03-13 11:37:20 +0100

    Logout user

--- backend/controllers/authenticationController.js
+++ backend/controllers/authenticationController.js
@@ -84,7 +84,39 @@ const loginUser = async (req, res) => {
 	}
 };
 
+const logoutUser = (req, res) => {
+	try {
+		const session = req.cookies[sessionKey];
+
+		if (session) {
+			User.findOne({ session })
+				.then(foundUser => {
+					if (foundUser) {
+						User.updateOne({ session }, { $unset: { session } })
+							.then(() => {
+								res.clearCookie(sessionKey);
+								return res.status(200).json({ message: "User successfully logged out" });
+							})
+							.catch(error => {
+								return res.status(400).json({ error: error.message });
+							});
+					} else {
+						return res.status(400).json({ error: "No user found with the given session id" });
+					}
+				})
+				.catch(error => {
+					return res.status(400).json({ error: error.message });
+				});
+		} else {
+			return res.status(400).json({ error: "The requested session does not exist" });
+		}
+	} catch (error) {
+		return res.status(500).json({ error: error.message });
+	}
+};
+
 module.exports = {
 	registerUser,
 	loginUser,
+	logoutUser,
 };
--- backend/routes/authenticationRoutes.js
+++ backend/routes/authenticationRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/authenticationController");
+const { registerUser, loginUser, logoutUser } = require("../controllers/authenticationController");
 
 const router = express.Router();
 
 router.post("/api/user/register", registerUser);
 router.post("/api/user/login", loginUser);
+router.get("/api/user/logout", logoutUser);
 
 module.exports = router;
