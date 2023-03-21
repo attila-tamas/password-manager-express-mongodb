419a75bf53dcbf77669fc5efea74e8d903bd654a
attila-tamas <tamasattila02@gmail.com>
2023-03-21 16:14:02 +0100

    Refactor authenticationController.js

--- backend/controllers/authenticationController.js
+++ backend/controllers/authenticationController.js
@@ -7,29 +7,17 @@ const registerUser = async (req, res) => {
 	try {
 		if (req.body.email && req.body.password) {
 			const email = req.body.email;
-
-			User.findOne({ email })
-				.then(async foundUser => {
-					if (!foundUser) {
-						const password = await bcrypt.hash(req.body.password, 10);
-
-						User.create({
-							email,
-							password,
-						})
-							.then(savedUser => {
-								return res.json(savedUser);
-							})
-							.catch(error => {
-								return res.json({ error: error.message });
-							});
-					} else {
-						return res.status(400).json({ error: "The given email address is already in use" });
-					}
-				})
-				.catch(error => {
-					return res.json({ error: error.message });
+			const user = await User.findOne({ email });
+			if (!user) {
+				const password = await bcrypt.hash(req.body.password, 10);
+				const createdUser = await User.create({
+					email,
+					password,
 				});
+				return res.status(200).json(createdUser);
+			} else {
+				return res.status(400).json({ error: "The given email address is already in use" });
+			}
 		} else {
 			return res.status(400).json({ error: "Incorrect input" });
 		}
@@ -42,40 +30,21 @@ const loginUser = async (req, res) => {
 	try {
 		if (req.body.email && req.body.password) {
 			const email = req.body.email;
-
-			User.findOne({ email })
-				.then(async foundUser => {
-					if (foundUser) {
-						const arePasswordsEqual = await bcrypt.compare(req.body.password, foundUser.password);
-
-						if (arePasswordsEqual) {
-							const session = uuid.v4(); // generate a session id
-
-							res.cookie(sessionKey, session);
-
-							User.updateOne({ email }, { $set: { session } })
-								.then(() => console.log("User session successfully updated"))
-								.catch(error => {
-									console.log(error);
-								});
-
-							const userData = {
-								email: foundUser.email,
-								session,
-								_id: foundUser._id,
-							};
-
-							return res.status(200).json(userData);
-						} else {
-							return res.status(400).json({ error: "Incorrect password" });
-						}
-					} else {
-						return res.status(400).json({ error: "No account found with the given email address" });
-					}
-				})
-				.catch(error => {
-					return res.json({ error: error.message });
-				});
+			const user = await User.findOne({ email });
+			if (user) {
+				const arePasswordsEqual = await bcrypt.compare(req.body.password, user.password);
+				if (arePasswordsEqual) {
+					const session = uuid.v4(); // generate a session id
+					res.cookie(sessionKey, session);
+					await User.updateOne({ email }, { $set: { session } }).then(() => {
+						return res.status(200).json(user);
+					});
+				} else {
+					return res.status(400).json({ error: "Incorrect password" });
+				}
+			} else {
+				return res.status(400).json({ error: "No account found with the given email address" });
+			}
 		} else {
 			return res.status(400).json({ error: "Incorrect input" });
 		}
@@ -84,29 +53,19 @@ const loginUser = async (req, res) => {
 	}
 };
 
-const logoutUser = (req, res) => {
+const logoutUser = async (req, res) => {
 	try {
 		const session = req.cookies[sessionKey];
-
 		if (session) {
-			User.findOne({ session })
-				.then(foundUser => {
-					if (foundUser) {
-						User.updateOne({ session }, { $unset: { session } })
-							.then(() => {
-								res.clearCookie(sessionKey);
-								return res.status(200).json({ message: "User successfully logged out" });
-							})
-							.catch(error => {
-								return res.status(400).json({ error: error.message });
-							});
-					} else {
-						return res.status(400).json({ error: "No user found with the given session id" });
-					}
-				})
-				.catch(error => {
-					return res.status(400).json({ error: error.message });
+			const user = await User.findOne({ session });
+			if (user) {
+				await User.updateOne({ session }, { $unset: { session } }).then(() => {
+					res.clearCookie(sessionKey);
+					return res.status(200).json({ message: "User successfully logged out" });
 				});
+			} else {
+				return res.status(400).json({ error: "No user found with the given session id" });
+			}
 		} else {
 			return res.status(400).json({ error: "The requested session does not exist" });
 		}
@@ -115,26 +74,16 @@ const logoutUser = (req, res) => {
 	}
 };
 
-const getCurrentUser = (req, res) => {
+const getCurrentUser = async (req, res) => {
 	try {
 		const session = req.cookies[sessionKey];
-
 		if (session) {
-			User.findOne({ session })
-				.then(foundUser => {
-					if (foundUser) {
-						const userData = {
-							email: foundUser.email,
-						};
-
-						return res.status(200).json(userData);
-					} else {
-						return res.status(400).json({ error: "No user found with the given session id" });
-					}
-				})
-				.catch(error => {
-					return res.status(400).json({ error: error.message });
-				});
+			const user = await User.findOne({ session });
+			if (user) {
+				return res.status(200).json(user);
+			} else {
+				return res.status(400).json({ error: "No user found with the given session id" });
+			}
 		} else {
 			return res.status(400).json({ error: "The requested session does not exist" });
 		}
